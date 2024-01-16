0b097735268cb3a8675f9ca4713fb74d35ba6a83
Abdullah9715 <muftimuhammadabdullah225@gmail.com>
2024-01-16 09:51:52 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Routese from './routes/Routese';
+// import Footer from './Components/Footer/Footer';
+
+function App() {
+  return (
+   <>
+   
+       <Routese/>
+       
+   </>
+  );
+}
+
+export default App;
